6049dcd9b645df1ed6a95baa8f0fb8d5789e15ef
BastiaanMeijer2002 <bastiaanmeijer2002@outlook.com>
2022-11-24 22:24:30 +0100

    added support for routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import TodoContainer from "./components/Todocontainer"
 
 import "./app.css"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<TodoContainer />}/>
+        </Routes>
+    </BrowserRouter>, 
+    document.getElementById("root")
+)
